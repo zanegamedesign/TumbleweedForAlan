0a13c25860b7cef16bbe625e1d47802fa9fef4eb
zanegamedesign <52474563+zanegamedesign@users.noreply.github.com>
2019-08-21 17:05:06 +1000

    initial commit

---
+++ Assets/Low Poly Western Saloon/Prefabs/SecondCharacterScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SecondCharacterScript : MonoBehaviour
+{
+    int health = 16;
+
+    public Transform tntSpawner;
+    public float shot_delay = 2f;
+    public GameObject bullet_object;
+    private float shot_cooldown = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= shot_cooldown && Input.GetKey(KeyCode.K))
+        {
+            Shoot();
+        }
+
+        void Shoot()
+        {
+            Instantiate(bullet_object, tntSpawner.position, transform.rotation);
+            shot_cooldown = Time.time + shot_delay;
+        }
+    }
+}
